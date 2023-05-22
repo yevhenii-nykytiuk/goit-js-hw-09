e533889f2d83d2970519bea643471f9f5f3dd35d
Yevhenii Nykytiuk <crashbandicoot123r@gmail.com>
2023-05-23 01:57:16 +0300

    added task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const colorStart = document.querySelector('button[data-start]');
+const colorStop = document.querySelector('button[data-stop]');
+
+colorStart.addEventListener("click", hendlerOnClickColorStart);
+colorStop.addEventListener("click", hendlerOnClickColorStop);
+
+let intervalId = null;
+
+function hendlerOnClickColorStart({target}) {
+
+  target.setAttribute("disabled", true);
+  colorStop.removeAttribute("disabled");
+
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 500)
+}
+
+function hendlerOnClickColorStop({target}) {
+  target.setAttribute("disabled", true);
+  colorStart.removeAttribute("disabled");
+  clearInterval(intervalId);
+}
+
