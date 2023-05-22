01e8382bb99981db80f6a50c4f83bacf6ef8efef
yevhenii-nykytiuk <76697642+yevhenii-nykytiuk@users.noreply.github.com>
2023-05-22 23:57:41 +0000

    Deploying to gh-pages from @ yevhenii-nykytiuk/goit-js-hw-09@e533889f2d83d2970519bea643471f9f5f3dd35d 🚀

---
+++ 01-color-switcher.93b034ee.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function({target:t}){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),500)})),e.addEventListener("click",(function({target:e}){e.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(r)}));let r=null;
+//# sourceMappingURL=01-color-switcher.93b034ee.js.map
